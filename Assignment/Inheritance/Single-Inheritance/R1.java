168fe80802831955e1d391fd3f690bda5deaa216
Akash-Naickar <akashnaikar2007@gmail.com>
2025-03-18 15:04:20 +0530

    Update R1.java

--- Assignment/Inheritance/Single-Inheritance/R1.java
+++ Assignment/Inheritance/Single-Inheritance/R1.java
@@ -5,15 +5,15 @@ System.out.println("Animal is eating");
 }
 
 public class Lion extends Animal{
-public void bark(){
-System.out.println("Dog barking");
+public void roar(){
+System.out.println("Lion roaring");
 }
 }
 
 public class Main{
 public static void main(String[] args){
-Dog mine = new Dog();
+Lion mine = new Lion();
 mine.eat();
-mine.bark();
+mine.roar();
 }
 }
