68c5fdbd61994d2d9ac8acb6f817f562056373c8
Akash-Naickar <akashnaikar2007@gmail.com>
2025-03-18 12:25:27 +0530

    Update R2.java

--- Assignment/Inheritance/Multi-level-Inheritance/R2.java
+++ Assignment/Inheritance/Multi-level-Inheritance/R2.java
@@ -1,24 +1,24 @@
 public class Adjudication {
 public void AR() {
-System.out.println("Assault Rifle Action");
+System.out.println("Click Left-mouse for Assault Rifle Action");
 }
 }
 
 public class Deliverance extends Adjudication{
 public void SG(){
-System.out.println("Shot Gun Action");
+System.out.println("Click Left-mouse for Shot Gun Action");
 }
 }
 
 public class Spl1 extends Deliverance{
 public void SpecAb1(){
-System.out.println("Vantage Connection");
+System.out.println("Press Shift for Vantage Connection");
 }
 }
 
 public class Spl2 extends Spl1{
 public void SpecAb2(){
-System.out.println("Culling Turret");
+System.out.println("Press E for Culling Turret");
 }
 }
 
