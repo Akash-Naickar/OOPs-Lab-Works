7c6a275a1f1681b8811096d795dc7de7dd4cc051
Akash-Naickar <akashnaikar2007@gmail.com>
2025-03-18 13:10:28 +0530

    Update R1.java

--- Assignment/Data-Abstraction/R1.java
+++ Assignment/Data-Abstraction/R1.java
@@ -1,1 +1,28 @@
-
+abstract class Ult {
+public abstract void displayUlt();
+public void showUltType() {
+System.out.println("Ultimate Ability");
+}
+}
+class Passive extends Ult{
+public void displayUlt() {
+System.out.println("Ult: Final Judgement");
+}
+public void displayPassive(){
+System.out.println("Passive: Regeneration");
+}
+}
+class heroStat extends Passive{
+public void displayName(){
+System.out.println("Name: The Punisher");
+}
+}
+public class Main{
+public static void main(String[] args){
+heroStat mine = new heroStat();
+mine.displayUlt();
+mine.displayPassive();
+mine.displayName();
+mine.showUltType();
+}
+}
