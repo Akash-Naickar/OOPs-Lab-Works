0eaef3e08f36d9c9cc0241bb967705ef88560e5e
Akash-Naickar <akashnaikar2007@gmail.com>
2025-04-03 11:47:20 +0530

    Create R2.java
    traversing through array

---
+++ Assignment/Arrays/R2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Mian
+{
+public static void main(String[] args)
+{
+    String[] arr = {"GER", "Stone Ocean", "Weather Rapport"};
+    for(int i = 0; i<arr.length; i++)
+    {
+        System.out.println("The "+(i+1)+" th element is "+arr[i]);
+    }
+}
+}
