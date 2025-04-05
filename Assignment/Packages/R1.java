d2a77a823c808a84311dd8d708366bfe0611718a
Akash-Naickar <akashnaikar2007@gmail.com>
2025-04-05 22:25:59 +0530

    Update R1.java

--- Assignment/Packages/R1.java
+++ Assignment/Packages/R1.java
@@ -1,1 +1,24 @@
+package mypack2;
+
+public class Main {
+    double length, width;
+
+    public Main(double l, double w) {
+        length = l;
+        width = w;
+    }
+
+    public double area() {
+        return length * width;
+    }
+}
+
+import mypack2.Main;
+
+public class Test2 {
+    public static void main(String[] args) {
+        Main r = new Main(5, 3.5);
+        System.out.println("Area: " + r.area());
+    }
+}
 
