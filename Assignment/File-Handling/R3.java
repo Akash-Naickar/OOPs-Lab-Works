7516c6c34ef9cd8fdec086041fad5e2cd9f2d28f
Akash-Naickar <akashnaikar2007@gmail.com>
2025-04-05 22:23:52 +0530

    Create R3.java

---
+++ Assignment/File-Handling/R3.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            File file = new File("output1.txt");
+            Scanner reader = new Scanner(file);
+            while (reader.hasNextLine()) {
+                String data = reader.nextLine();
+                System.out.println(data);
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+        }
+    }
+}
